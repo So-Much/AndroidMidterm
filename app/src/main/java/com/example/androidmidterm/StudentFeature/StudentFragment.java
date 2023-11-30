2a13fca7b0fd53406d145fba40011de6a9e3c237
MinhNhieu <52100456@student.tdtu.edu.vn>
2023-11-30 10:38:45 +0700

    done

--- app/src/main/java/com/example/androidmidterm/StudentFeature/StudentFragment.java
+++ app/src/main/java/com/example/androidmidterm/StudentFeature/StudentFragment.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -171,11 +173,18 @@ public class StudentFragment extends Fragment {
         fabImportStudentCSV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent filePicker = new Intent(Intent.ACTION_GET_CONTENT);
-                filePicker.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-                filePicker.setType("*/*");
-                filePicker = Intent.createChooser(filePicker, "Select a file");
-                filePickerLauncher.launch(filePicker);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                    if (!Environment.isExternalStorageManager()) {
+                        Intent intent = new Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                        startActivity(intent);
+                    } else {
+                        Intent filePicker = new Intent(Intent.ACTION_GET_CONTENT);
+                        filePicker.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                        filePicker.setType("*/*");
+                        filePicker = Intent.createChooser(filePicker, "Select a file");
+                        filePickerLauncher.launch(filePicker);
+                    }
+                }
             }
         });
 //        Export
